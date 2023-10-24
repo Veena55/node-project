e34701d8a0b8631a9d098c2c274c73a839bee911
Veena Rao <veena.500apps@gmail.com>
2023-10-24 14:10:20 +0530

    setup ejs engine

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));  
 
+//setp ejs engine for views
+app.set('view engine','ejs');
+//path for views
+app.set('views','./views');
+
 app.listen(port, (err)=>{
     if(err) {
         console.log("Something went wrong");
