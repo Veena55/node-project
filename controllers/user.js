3d4071024c2b17648fdbb29329e71c08a62c0d09
Veena Rao <veena.500apps@gmail.com>
2023-10-26 17:28:57 +0530

    added passport js

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,26 @@
 const User = require('../models/users');
 module.exports.profile = (req,res) => {
+    if(!req.cookies.user_id) {
+        return res.redirect('/users/signin');
+    }
     return res.render('users',{
         title : "Profile",
         description: "My Name is Veena ! I love to do coding 👩🏽‍💻👩‍💻"
     });
 }
 
-module.exports.sign_in = function (req,res) {
-    return res.render('sign_in',{
-        title: "Sign In"
-    });
+module.exports.sign_in = async (req,res) => {
+    try {
+        if(req.cookies.user_id) {
+            let user = await User.findById(req.cookies.user_id);
+            console.log(user);
+            if(user) return res.redirect('/users/profile');
+        }
+            return res.render('sign_in',{title: "Sign In"})
+        
+    } catch (error) {
+        console.log("Something went wrong!",error);
+    }   
 }
 
 module.exports.sign_up = function (req,res) {
@@ -28,19 +39,26 @@ module.exports.create = async (req,res) => {
         let user = await User.findOne({email: req.body.email});     
          if(!user) {
              const result = await User.create(req.body);
-             console.log(result);
         return res.redirect('/users/signin');
     } else {
         return res.redirect('back');
     }
     } catch (error) {
-        console.log("SOmething went wrong while signup", error);
+        console.log("Something went wrong while signup", error);
     }
     
-//    }
+
 }
 
 //sign in and create a session for the user
-module.exports.createSession = (req,res) => {
-    //To do later
+module.exports.createSession = async(req,res) => {
+    let user = await User.findOne({email: req.body.email});
+    if(user) {
+       if(user.password == req.body.password) {
+           res.cookie('user_id', user.id)
+           return res.redirect('/users/profile');
+    } 
+}
+        return res.redirect('back');
+
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ router.get('/profile',userController.profile);
 router.get('/signin',userController.sign_in);
 router.get('/signup',userController.sign_up);
 router.post('/create', userController.create);
+router.post('/create-session', userController.createSession);
+
 
 
 module.exports = router;
